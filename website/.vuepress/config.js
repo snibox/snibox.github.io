bd6be024710a3475e31d8817d2c59edbb45fd972
Vladimir Avgustov <vavgustov@gmail.com>
2018-07-14 15:37:31 +0300

    Improved meta and link tags.

--- website/.vuepress/config.js
+++ website/.vuepress/config.js
@@ -1,8 +1,28 @@
 module.exports = {
   base: '',
   title: 'Snibox',
-  description: 'Self-hosted code snippets manager.',
+  description: 'A self-hosted snippet manager',
   head: [
+    ['meta', { property: 'og:type', content: 'article' }],
+    ['meta', { property: 'og:title', content: 'Snibox' }],
+    ['meta', { property: 'og:description', content: 'A self-hosted snippet manager' }],
+    ['meta', { property: 'og:image', content: '/logo.png' }],
+
+    ['meta', { name: 'twitter:card', content: 'summary' }],
+    ['meta', { name: 'twitter:title', content: 'Snibox' }],
+    ['meta', { name: 'twitter:description', content: 'A self-hosted snippet manager' }],
+    ['meta', { name: 'twitter:image', content: '/logo.png' }],
+
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icons/apple-touch-icon.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-touch-icons/apple-touch-icon-57x57.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-touch-icons/apple-touch-icon-72x72.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icons/apple-touch-icon-76x76.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-touch-icons/apple-touch-icon-114x114.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icons/apple-touch-icon-120x120.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-touch-icons/apple-touch-icon-144x144.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icons/apple-touch-icon-180x180.png' }],
+
     ['link', { rel: 'icon', href: '/favicon.png' }]
   ],
   ga: 'UA-122253325-1',
